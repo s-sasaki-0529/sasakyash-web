858bf265b6f11c562635228aad8a59c06ff18881
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-22 00:31:49 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'sasakyash',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: process.env.FIREBASE_APP_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        },
+        services: {
+          firestore: true,
+          functions: {
+            location: 'us-central1',
+            emulatorPort: 5001,
+            emulatorHost: 'http://localhost',
+          },
+        },
+      },
+    ],
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">{{ message }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ app }) {
+    const helloWorld = app.$fire.functions.httpsCallable('helloWorld')
+    const message = await helloWorld().then((res) => {
+      return res.data.message
+    })
+    return { message }
+  },
+  data: () => ({
+    message: '',
+  }),
+})
+</script>
+
+<style></style>

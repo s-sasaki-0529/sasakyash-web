f08c26c2ed1f6368323df4176b702df535ae5202
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-23 00:43:15 +0900

    今月の公費、私費の残り予算を画面に出せるところまで

---
+++ components/index/balanceBox.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="balance-box box">
+    <div class="today-label title is-4">{{ formattedDate }}</div>
+    <div class="budget-info">
+      <div class="public">
+        <span>公費 残り</span>
+        <span>{{ publicBalance }}</span>
+        <span>円</span>
+      </div>
+      <div class="public">
+        <span>私費 残り</span>
+        <span>{{ privateBalance }}</span>
+        <span>円</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+
+export default {
+  props: {
+    publicBalance: {
+      type: Number,
+      required: true,
+    },
+    privateBalance: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    formattedDate() {
+      return dayjs().format('YYYY年MM月DD日')
+    },
+    privateBudget() {
+      return Number(process.env.privateBudget)
+    },
+    publicBudget() {
+      return Number(process.env.publicBudget)
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="container">Hello, World!!</div>
+  <div class="container">
+    <balance-box
+      :publicBalance="currentMonthPublicBalance"
+      :privateBalance="currentMonthPrivateBalance"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import balanceBox from '~/components/index/balanceBox.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: { balanceBox },
+  async asyncData({ app }) {
+    const balance = await app.$fire.functions
+      .httpsCallable('balance')()
+      .then((res) => res.data)
+    return {
+      currentMonthPrivateBalance: balance.private,
+      currentMonthPublicBalance: balance.public,
+    }
+  },
+  data: () => ({
+    currentMonthPrivateBalance: 0,
+    currentMonthPublicBalance: 0,
+  }),
+})
 </script>
 
 <style></style>

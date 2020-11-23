2d17402987af0d2b21fa1b57dbe98800cc1f178e
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-23 19:52:57 +0900

    私費の支出状況コンポーネントに対して年月をpropsで渡せるように

--- pages/analytics/private.vue
+++ pages/analytics/private.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="analytics-private">私費詳細分析画面</div>
+  <div class="analytics-private">
+    <daily-private-balance-chart :year="2020" :month="9" :height="500" />
+  </div>
 </template>
 
 <script>

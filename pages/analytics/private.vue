b35dedd2359b253cc2fcae739e8b06aa1fe646bc
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-29 17:42:33 +0900

    自動デプロイを確認するための変更

--- pages/analytics/private.vue
+++ pages/analytics/private.vue
@@ -1,5 +1,5 @@
 <template>
-  <daily-balance-chart-board title="[公費] 今月の支出状況" paymentType="public" />
+  <daily-balance-chart-board title="[公費] 今月の残額遷移" paymentType="public" />
 </template>
 
 <script>

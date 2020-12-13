153806bebe8fa2d966420bb8925e54877d63a8ec
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-12-13 19:54:40 +0900

    水道・光熱費をテーブルで表示するように

--- pages/list/utilitiesBills.vue
+++ pages/list/utilitiesBills.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="list-utilities-bills">水道光熱費</div>
+  <div class="list-utilities-bills">
+    <div class="table-container">
+      <table class="table is-fullwidth">
+        <thead>
+          <tr>
+            <th>年月</th>
+            <th>電気</th>
+            <th>ガス</th>
+            <th>水道</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :key="date" v-for="date in Object.keys(bills)">
+            <td>{{ date }}</td>
+            <td>{{ bills[date].electric || '-' }}</td>
+            <td>{{ bills[date].gas || '-' }}</td>
+            <td>{{ bills[date].water || '-' }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    bills: {}
+  }),
+  mounted() {
+    const api = this.$fire.functions.httpsCallable('utilityBills')
+    return api().then(res => {
+      this.bills = res.data
+    })
+  }
+}
 </script>
 
 <style lang="scss"></style>

b8a3882013e71ea007f611307b00215b01a0200d
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-04-05 00:02:32 +0900

    メニューを一回層に

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,28 +8,12 @@
 
             <ul class="menu-list">
               <li><nuxt-link to="/">ダッシュボード</nuxt-link></li>
-            </ul>
-
-            <p class="menu-label">分析</p>
-            <ul class="menu-list">
               <li><nuxt-link to="/analytics/public">公費詳細</nuxt-link></li>
               <li><nuxt-link to="/analytics/private">私費詳細</nuxt-link></li>
-            </ul>
-
-            <p class="menu-label">一覧</p>
-            <ul class="menu-list">
               <li><nuxt-link to="/list/utilitiesBills">水道・光熱費</nuxt-link></li>
               <li><nuxt-link to="/list/haircut">散髪</nuxt-link></li>
-            </ul>
-
-            <p class="menu-label">入力補助</p>
-            <ul class="menu-list">
               <li><nuxt-link to="/forms/payment">クイック入力</nuxt-link></li>
               <li><nuxt-link to="/forms/subscription">サブスクリプション</nuxt-link></li>
-            </ul>
-
-            <p class="menu-label">その他</p>
-            <ul class="menu-list">
               <li><nuxt-link to="/others/setting">設定</nuxt-link></li>
             </ul>
           </div>
